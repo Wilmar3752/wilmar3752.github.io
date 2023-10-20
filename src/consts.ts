d119b8fd270f2b86ac99393deb33772416f089e2
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2023-10-20 18:40:19 -0500

    testing

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
-export const SITE_NAME = "NimbusNarratives";
-export const SITE_TITLE = "NimbusNarratives | A personal blog by Abdullah Elsayed (@abdllahdev)";
+export const SITE_NAME = "WILMAR";
+export const SITE_TITLE = "WILMAR | A personal blog by Abdullah Elsayed (@abdllahdev)";
 export const SITE_DESCRIPTION =
-  "NimbusNarratives | A personal blog by Abdullah Elsayed (@abdllahdev)";
+  "WILMAR | A personal blog by Abdullah Elsayed (@abdllahdev)";
 export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
 export const GITHUB_HANDLE = "https://github.com/abdllahdev";
 export const CLOUDINARY_NAME = "example";
