3c17aaf547268c8bae0b3e70cf30a6ffbda4c8d9
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2023-10-23 21:50:19 -0500

    update template

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
-export const SITE_NAME = "WILMAR";
-export const SITE_TITLE = "WILMAR | A personal blog by Abdullah Elsayed (@abdllahdev)";
+export const SITE_NAME = "Data Science theory and practice";
+export const SITE_TITLE = "WILMAR | A personal blog by Wilmar Elsayed (@abdllahdev)";
 export const SITE_DESCRIPTION =
-  "WILMAR | A personal blog by Abdullah Elsayed (@abdllahdev)";
+  "WILMAR | A personal blog by Wilmar Elsayed (@abdllahdev)";
 export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
 export const GITHUB_HANDLE = "https://github.com/abdllahdev";
 export const CLOUDINARY_NAME = "example";
